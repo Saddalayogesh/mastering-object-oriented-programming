c7172a9fa64e27e1287c65f83ff156ca83f91ef4
Saddala Yogesh <saddalayogesh@gmail.com>
2026-06-17 23:55:02 +0530

    ✅ Add solution for: Recursive Factorial Calculation

---
+++ exercises/RecursiveFactorialCalculation.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n < 0 || n > 12) {
+            System.out.println("Invalid input");
+        } else {
+            System.out.println(factorial(n));
+        }
+
+        sc.close();
+    }
+}
